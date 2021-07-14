852cf92efe2bc85ea22a2458548422e7f542fae6
Maydur Rahman <mrahman@optionmetrics.com>
2021-07-14 14:38:36 -0400

    Test

---
+++ src/main/java/com/optionmetrics/demo/RestControllerService.java
@@ -0,0 +1,17 @@
+package com.optionmetrics.demo;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("/")
+public class RestControllerService {
+
+    @GetMapping(name = "test", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity helloWorld() {
+        return ResponseEntity.ok().body("{\"hello\": \"world\"}");
+    }
+
+}
